8b15448f60c1dc9328e35eb2b86030815cade619
core.maintainer@reflexmev.com <core.maintainer@reflexmev.com>
2025-09-16 12:39:30 +0100

    Deploy website - based on 8e4292886205e2b2e57f94ea606ea21b8dad0cff

--- sdk/src/index.ts
+++
@@ -1,8 +0,0 @@
-// Main exports
-export * from "./lib";
-export * from "./types";
-export * from "./constants";
-export * from "./utils";
-
-// Default export
-export { ReflexSDK as default } from "./lib";
