66517835b2152300da3961a7bdf6159b45d9f15a
reflex-mev-core <alon.benlavy@lotusresear.ch>
2025-11-04 12:19:04 +0000

    [FEAT] Add swap proxy (#9)

--- core/src/integrations/plugin/algebra/full/BasePluginV3Factory.sol
+++ core/src/integrations/plugin/algebra/full/BasePluginV3Factory.sol
@@ -2,34 +2,32 @@
 pragma solidity =0.8.20;
 
 import "@cryptoalgebra/core/interfaces/IAlgebraFactory.sol";
-import "@cryptoalgebra/plugin/base/AlgebraFeeConfiguration.sol";
-import "@cryptoalgebra/plugin/interfaces/IBasePluginV1Factory.sol";
-import "./AlgebraBasePluginV1.sol";
+import "../interfaces/IBasePluginV3Factory.sol";
+import "./AlgebraBasePluginV3.sol";
 
-/// @title Algebra V1-based plugin factory
-/// @notice This contract creates Algebra V1-based plugins with ReflexAfterSwap functionality for Algebra liquidity pools
+/// @title Algebra Integral 1.2.2 default plugin factory
+/// @notice This contract creates Algebra sliding fee plugins for Algebra liquidity pools
 /// @dev This plugin factory can only be used for Algebra base pools
-contract BasePluginV1Factory is IBasePluginV1Factory {
-    /// @inheritdoc IBasePluginV1Factory
+contract BasePluginV3Factory is IBasePluginV3Factory {
+    /// @inheritdoc IBasePluginV2Factory
     bytes32 public constant override ALGEBRA_BASE_PLUGIN_FACTORY_ADMINISTRATOR =
         keccak256("ALGEBRA_BASE_PLUGIN_FACTORY_ADMINISTRATOR");
 
-    /// @inheritdoc IBasePluginV1Factory
+    /// @inheritdoc IBasePluginV2Factory
     address public immutable override algebraFactory;
 
-    /// @inheritdoc IBasePluginV1Factory
+    /// @inheritdoc IBasePluginV2Factory
     address public override farmingAddress;
 
-    /// @notice Internal storage for the default fee configuration
-    AlgebraFeeConfiguration private _defaultFeeConfiguration;
+    /// @inheritdoc IBasePluginV2Factory
+    uint16 public override defaultBaseFee = 3000;
 
-    /// @notice The ReflexRouter address for plugins created by this factory
     address public reflexRouter;
 
     /// @notice Configuration ID for profit distribution used by plugins created by this factory
     bytes32 public reflexConfigId;
 
-    /// @inheritdoc IBasePluginV1Factory
+    /// @inheritdoc IBasePluginV2Factory
     mapping(address poolAddress => address pluginAddress) public override pluginByPool;
 
     modifier onlyAdministrator() {
@@ -44,11 +42,6 @@ contract BasePluginV1Factory is IBasePluginV1Factory {
         algebraFactory = _algebraFactory;
         reflexRouter = _reflexRouter;
         reflexConfigId = _configId;
-
-        // Set default fee configuration similar to V1
-        _defaultFeeConfiguration = AlgebraFeeConfiguration({
-            alpha1: 2900, alpha2: 12000, beta1: 360, beta2: 60000, gamma1: 59, gamma2: 8500, baseFee: 100
-        });
     }
 
     /// @inheritdoc IAlgebraPluginFactory
@@ -66,7 +59,7 @@ contract BasePluginV1Factory is IBasePluginV1Factory {
         require(msg.sender == algebraFactory);
     }
 
-    /// @inheritdoc IBasePluginV1Factory
+    /// @inheritdoc IBasePluginV2Factory
     function createPluginForExistingPool(address token0, address token1) external override returns (address) {
         IAlgebraFactory factory = IAlgebraFactory(algebraFactory);
         require(factory.hasRoleOrOwner(factory.POOLS_ADMINISTRATOR_ROLE(), msg.sender));
@@ -79,56 +72,27 @@ contract BasePluginV1Factory is IBasePluginV1Factory {
 
     function _createPlugin(address pool) internal returns (address) {
         require(pluginByPool[pool] == address(0), "Already created");
-        AlgebraBasePluginV1 plugin = new AlgebraBasePluginV1(
-            pool, algebraFactory, address(this), _defaultFeeConfiguration, reflexRouter, reflexConfigId
-        );
+        AlgebraBasePluginV3 plugin =
+            new AlgebraBasePluginV3(pool, algebraFactory, address(this), defaultBaseFee, reflexRouter, reflexConfigId);
         pluginByPool[pool] = address(plugin);
         return address(plugin);
     }
 
-    /// @inheritdoc IBasePluginV1Factory
+    /// @inheritdoc IBasePluginV2Factory
     function setFarmingAddress(address newFarmingAddress) external override onlyAdministrator {
         require(farmingAddress != newFarmingAddress);
         farmingAddress = newFarmingAddress;
         emit FarmingAddress(newFarmingAddress);
     }
 
-    /// @inheritdoc IBasePluginV1Factory
-    function setDefaultFeeConfiguration(AlgebraFeeConfiguration calldata newConfig)
-        external
-        override
-        onlyAdministrator
-    {
-        // Validate the configuration
-        require(newConfig.gamma1 != 0 && newConfig.gamma2 != 0, "Gamma values must be > 0");
-        require(
-            uint256(newConfig.alpha1) + uint256(newConfig.alpha2) + uint256(newConfig.baseFee) <= type(uint16).max,
-            "Max fee too high"
-        );
-
-        _defaultFeeConfiguration = newConfig;
-        emit DefaultFeeConfiguration(newConfig);
+    /// @inheritdoc IBasePluginV2Factory
+    function setDefaultBaseFee(uint16 newDefaultBaseFee) external override onlyAdministrator {
+        require(defaultBaseFee != newDefaultBaseFee);
+        defaultBaseFee = newDefaultBaseFee;
+        emit DefaultBaseFee(newDefaultBaseFee);
     }
 
-    /// @inheritdoc IBasePluginV1Factory
-    function defaultFeeConfiguration()
-        external
-        view
-        override
-        returns (uint16 alpha1, uint16 alpha2, uint32 beta1, uint32 beta2, uint16 gamma1, uint16 gamma2, uint16 baseFee)
-    {
-        AlgebraFeeConfiguration memory config = _defaultFeeConfiguration;
-        return (config.alpha1, config.alpha2, config.beta1, config.beta2, config.gamma1, config.gamma2, config.baseFee);
-    }
-
-    /// @notice Get the default fee configuration as a struct
-    /// @return The default fee configuration struct
-    function getDefaultFeeConfigurationStruct() external view returns (AlgebraFeeConfiguration memory) {
-        return _defaultFeeConfiguration;
-    }
-
-    /// @notice Set the ReflexRouter address for plugins created by this factory
-    /// @param _reflexRouter The new ReflexRouter address
+    /// @inheritdoc IBasePluginV3Factory
     function setReflexRouter(address _reflexRouter) external onlyAdministrator {
         reflexRouter = _reflexRouter;
     }
