fcf17a1921df2c63614903479757e9eefb2c684c
reflex-mev-core <core.maintainer@reflexmev.com>
2025-11-04 14:01:29 +0200

    build fix

--- core/script/deploy-swap-proxy/DeployBackrunEnabledSwapProxy.s.sol
+++ core/script/deploy-swap-proxy/DeployBackrunEnabledSwapProxy.s.sol
@@ -3,7 +3,7 @@ pragma solidity =0.8.20;
 
 import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
-import {BackrunEnabledSwapProxy} from "@reflex/integrations/BackrunEnabledSwapProxy.sol";
+import {BackrunEnabledSwapProxy} from "@reflex/integrations/router/BackrunEnabledSwapProxy.sol";
 
 /**
  * @title DeployBackrunEnabledSwapProxy
